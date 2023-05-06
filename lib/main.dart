114dc3842d88f1ce9daaa7e9e75d4ddfac0120e4
admin <koratutkarsh310@gmail.com>
2023-05-06 10:44:30 +0530

    aj

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:re_exam/Screen/ApiCall_Screeen/Provider/home_provider.dart';
+import 'package:re_exam/Screen/ApiCall_Screeen/View/home_screen.dart';
 
 void main()
 {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => HomeProvider(),),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/':(context) => HomeScreen(),
+        },
+      ),
     ),
   );
 }
